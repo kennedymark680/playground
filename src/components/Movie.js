5d7d32a9cc8a6c6a6a06c3a2a1d3b8cb1f5cf4c2
Mark Kennedy <kennedymark@hotmail.com>
2022-06-03 22:41:35 -0400

    button input

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const Movie = () => {
-  return <div>Movie</div>
+  fetch(`https://api.themoviedb.org/3?api_key=dd80d4093c52a1f44c0690a18568bd3b`)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+
+  return (
+    <div>
+      <input></input>
+      <button>Search</button>
+    </div>
+  )
 }
 
 export default Movie
